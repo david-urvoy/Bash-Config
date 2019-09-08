4f0c005ad4387ac7a19387c587e24632a46d4f51
David Urvoy <daviid.urvoy@gmail.com>
2019-09-08 19:54:19 +0000

    powerline-fonts install depending on distro

--- zsh/install.sh
+++ zsh/install.sh
@@ -1,6 +1,13 @@
-#!/bin/zsh
+#!/bin/bash
 
-./powerline-fonts/install.sh
+distro=$(uname -a)
+if [[ $distro == *"Ubuntu"* ]] || [[ $distro == *"Debian"* ]]; then
+    sudo apt-get install zsh -y
+elif [[ $distro == *"Fedora"* ]]; then
+    sudo dnf install zsh -y
+fi
+
+. $CONFIG_ZSH/powerline-fonts/install.sh
 
 cp -r $CONFIG_ZSH/.zshrc ~/
 cp -r $CONFIG_ZSH/.zsh ~/
