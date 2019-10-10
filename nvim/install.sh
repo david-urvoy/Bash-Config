93aa59790bfd0f5ef46d8d8912afdfe3b534d194
David Urvoy <daviid.urvoy@gmail.com>
2019-10-10 18:54:25 +0000

    Remove vim autoload

--- nvim/install.sh
+++ nvim/install.sh
@@ -1,14 +1,8 @@
 NVIM_DIR=~/.config/nvim
-#AUTOLOAD_DIR=~/.local/share/nvim/site
 
 if [ $ENV_TYPE != "CONTAINER" ]; then sudo $PKG_MANAGER install neovim -y; fi
 
-
 # .vimrc
 cp $CONFIG_NVIM/.vimrc ~/
-# copy vim config
-# cp -r $CONFIG_NVIM/.vim ~/
 # copy nvim config
 mkdir -p $NVIM_DIR && cp -r $CONFIG_NVIM/init.vim $NVIM_DIR
-# copy vim-plug config
-# mkdir -p $AUTOLOAD_DIR && cp -r $CONFIG_NVIM/autoload $AUTOLOAD_DIR
