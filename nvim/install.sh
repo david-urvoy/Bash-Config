7cfc331307bf3493dd8d72ec3c1dc1a4f5488882
David Urvoy <daviid.urvoy@gmail.com>
2019-10-17 23:37:39 +0000

    Moved .zshrc content into init.vim for Neovim

--- nvim/install.sh
+++ nvim/install.sh
@@ -2,7 +2,5 @@ NVIM_DIR=~/.config/nvim
 
 if [ $ENV_TYPE != "CONTAINER" ]; then sudo $PKG_MANAGER install neovim -y; fi
 
-# .vimrc
-cp $CONFIG_NVIM/.vimrc ~/
 # copy nvim config
 mkdir -p $NVIM_DIR && cp -r $CONFIG_NVIM/init.vim $NVIM_DIR
