da310776e80c92a5e5f98316266ed409d6f2201a
David Urvoy <daviid.urvoy@gmail.com>
2019-11-14 21:51:54 +0000

    Add coc-settings installation

--- nvim/install.sh
+++ nvim/install.sh
@@ -4,3 +4,5 @@ if [ $ENV_TYPE != "CONTAINER" ]; then sudo $PKG_MANAGER install neovim -y; fi
 
 # copy nvim config
 mkdir -p $NVIM_DIR && cp -r $CONFIG_NVIM/init.vim $NVIM_DIR
+# copy coc config
+cp $CONFIG_NVIM/coc-settings.json $NVIM_DIR
