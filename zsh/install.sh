3ff4c26e3b36a386d02210c32c6cbeb072151a32
David Urvoy <daviid.urvoy@gmail.com>
2020-03-23 10:01:42 +0000

    Zsh installer install oh-my-zsh

--- zsh/install.sh
+++ zsh/install.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-
-if [ $ENV_TYPE != "CONTAINER" ]; then sudo $PKG_MANAGER install zsh -y; fi
-
-if [ $ENV_TYPE != "CONTAINER" ]; then sudo $PKG_MANAGER install fonts-powerline -y; fi
+if [ $ENV_TYPE != "CONTAINER" ]; then 
+    sudo $PKG_MANAGER install zsh -y;
+    sudo $PKG_MANAGER install fonts-powerline -y;
+    sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)";
+fi
 
 cp -r $CONFIG_ZSH/.zshrc ~/
 cp -r $CONFIG_ZSH/.zsh ~/
