32d6973e92514dffa058efc9cc3216e698cdc31f
David Urvoy <daviid.urvoy@gmail.com>
2020-03-23 09:54:53 +0000

    vim installer install Plug

--- nvim/install.sh
+++ nvim/install.sh
@@ -6,3 +6,5 @@ if [ $ENV_TYPE != "CONTAINER" ]; then sudo $PKG_MANAGER install neovim -y; fi
 mkdir -p $NVIM_DIR && cp -r $CONFIG_NVIM/init.vim $NVIM_DIR
 # copy coc config
 cp $CONFIG_NVIM/coc-settings.json $NVIM_DIR
+# install Plug
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
