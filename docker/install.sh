c6d00351a776cbfd043eca3ea55731d793031a8b
David Urvoy <david.urvoy.pro@gmail.com>
2020-11-24 16:55:46 +0000

    Add docker-compose install

--- docker/install.sh
+++ docker/install.sh
@@ -7,13 +7,13 @@ if [[ $PKG_MANAGER = "apt-get" ]]; then
 
     # docker engine
     sudo $PKG_MANAGER update
-    sudo $PKG_MANAGER install docker-ce docker-ce-cli containerd.io
+    sudo $PKG_MANAGER install docker-ce docker-ce-cli containerd.io docker-compose
 
 elif [[ $PKG_MANAGER = "dnf" ]]; then
     sudo dnf remove docker docker-client docker-client-latest docker-common docker-latest docker-latest-logrotate docker-logrotate docker-selinux docker-engine-selinux docker-engine
     sudo dnf -y install dnf-plugins-core
     sudo dnf config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
-    sudo dnf install docker-ce docker-ce-cli containerd.io
+    sudo dnf install docker-ce docker-ce-cli containerd.io docker-compose
 fi
 
 
