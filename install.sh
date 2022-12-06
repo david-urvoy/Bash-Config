bf609a680f46bcd16482a9dc5a80eb056bb1e990
David Urvoy <david.urvoy.pro@gmail.com>
2022-12-06 10:37:28 +0100

    only keep git, zsh and tmux

--- install.sh
+++ install.sh
@@ -1,27 +1,12 @@
 #!/bin/bash
 
-#if [[ -z $1 ]]; then
-	dev_home=$HOME/development
-#else
-#	dev_home=$1
-#fi
+dev_home=$HOME/development
 
 # init path variables
 . $dev_home/config/.path $dev_home
 
-# init distribution variables
-. $CONFIG_HOME/.distrib
-
-if [ $ENV_TYPE != "CONTAINER" ]; then sudo $PKG_MANAGER update; fi
-
 # install zsh
-#. $CONFIG_ZSH/install.sh
-
-# install tmux
-#if [ $ENV_TYPE != "CONTAINER" ]; then sudo $PKG_MANAGER install -y tmux; fi
-
-# install nvim
-#. $CONFIG_NVIM/install.sh
+. $CONFIG_ZSH/install.sh
 
 # install git
 . $CONFIG_GIT/install.sh
